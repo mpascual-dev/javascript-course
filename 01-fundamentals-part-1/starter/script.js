4167355dba9769c5df2b4526389cd40db64100e3
mpascual-dev <mheryllepascual05@gmail.com>
2025-09-02 22:34:25 +0800

    feat: add Part 2 starter files and documentation

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -161,7 +161,7 @@ console.log(ageJonas, ageSarah, averageAge);
 //     console.log(`Sarahh is too young. Wait another ${yearsLeft} years`);
 // }
  
-// // truthy and falsy values
+// // 
 // console.log(Boolean(0));
 // console.log(Boolean(undefined));
 // console.log(Boolean("Jonas"));
@@ -171,23 +171,23 @@ console.log(ageJonas, ageSarah, averageAge);
 // ////////////////////////////////////
 // // Coding Challenge #2
  
-// // Reuse your BMI calculation from Challenge #1
-// const massMark = 78;
-// const heightMark = 1.69;
-// const massJohn = 92;
-// const heightJohn = 1.95;
+// Reuse your BMI calculation from Challenge #1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
  
-// const BMIMark = massMark / heightMark ** 2;
-// const BMIJohn = massJohn / (heightJohn * heightJohn);
-// console.log(BMIMark, BMIJohn);
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
  
-// // Your if/else statement here:
-// // Compare BMIs and create intelligent messages
-// // Use template literals for beautiful output
-// if (BMIMark > BMIJohn) {
-//   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
-// } else {
-//   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
-// }
- //
- //
+// Your if/else statement here:
+// Compare BMIs and create intelligent messages
+// Use template literals for beautiful output
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
+ 
+ 
